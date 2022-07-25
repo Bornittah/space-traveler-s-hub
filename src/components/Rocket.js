d5711df352af587b4d9be0e9e9acef17fdf800ad
bornittah <bornittah@gmail.com>
2022-07-26 01:03:05 +0300

    Design UI

---
+++ src/components/Rocket.js
@@ -0,0 +1,16 @@
+const Rocket = () => (
+  <div className="rocket-row">
+    <div className="rocket-img">
+      <img src="./logo512.png" alt="rocket" />
+    </div>
+    <div className="rocket-desc">
+      <h3 className="title">Rocket 1</h3>
+      <div className="description">
+        it is a long established fact that a reader will be distracted by the readable content.
+      </div>
+      <button type="submit" className="btn">Reserve Rocket</button>
+    </div>
+  </div>
+);
+
+export default Rocket;
---
+++ src/components/RocketList.js
@@ -0,0 +1,13 @@
+import Rocket from './Rocket';
+
+const RocketList = () => (
+  <section className="section">
+    <ul className="list">
+      <li>
+        <Rocket />
+      </li>
+    </ul>
+  </section>
+);
+
+export default RocketList;
